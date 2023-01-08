382a1420a3a7620c52aa73a666c0de7e85e94363
Sarugot <114975317+Sarugot@users.noreply.github.com>
2023-01-09 01:09:54 +0500

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,14 @@
+class MissingTokensError(Exception):
+    pass
+
+
+class StatusCodeError(Exception):
+    pass
+
+
+class HomeworkError(Exception):
+    pass
+
+
+class StatusError(Exception):
+    pass
